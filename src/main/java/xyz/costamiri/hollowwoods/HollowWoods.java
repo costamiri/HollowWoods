ff44e6aa2eb757e36ad2055f30ee623bb83c2ff4
costamiri <dennisdelacosta@gmail.com>
2021-12-06 21:48:17 +0100

    added other wood logs

--- src/main/java/xyz/costamiri/hollowwoods/HollowWoods.java
+++ src/main/java/xyz/costamiri/hollowwoods/HollowWoods.java
@@ -9,23 +9,56 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import xyz.costamiri.hollowwoods.blocks.HollowLog;
 
 public class HollowWoods implements ModInitializer {
 	public static final String modid = "hollowwoods";
-	public static final Logger LOGGER = LogManager.getLogger("hollowwoods");
+	// public static final Logger LOGGER = LogManager.getLogger("hollowwoods");
 	public static final HollowLog HOLLOW_OAK_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog HOLLOW_SPRUCE_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog HOLLOW_BIRCH_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog HOLLOW_JUNGLE_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog HOLLOW_ACACIA_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog HOLLOW_DARK_OAK_LOG
 			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
 
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.BLOCK,
 				new Identifier(modid, "hollow_oak_log"), HOLLOW_OAK_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "hollow_spruce_log"), HOLLOW_SPRUCE_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "hollow_birch_log"), HOLLOW_BIRCH_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "hollow_jungle_log"), HOLLOW_JUNGLE_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "hollow_acacia_log"), HOLLOW_ACACIA_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "hollow_dark_oak_log"), HOLLOW_DARK_OAK_LOG);
 
 		Registry.register(Registry.ITEM,
 				new Identifier(modid, "hollow_oak_log"),
 				new BlockItem(HOLLOW_OAK_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "hollow_spruce_log"),
+				new BlockItem(HOLLOW_SPRUCE_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "hollow_birch_log"),
+				new BlockItem(HOLLOW_BIRCH_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "hollow_jungle_log"),
+				new BlockItem(HOLLOW_JUNGLE_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "hollow_acacia_log"),
+				new BlockItem(HOLLOW_ACACIA_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "hollow_dark_oak_log"),
+				new BlockItem(HOLLOW_DARK_OAK_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
 }
