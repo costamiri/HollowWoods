e8f12b07fc3d30f27864f43de8f2ee809121092c
costamiri <dennisdelacosta@gmail.com>
2021-12-07 11:06:58 +0100

    added stripped variants

--- src/main/java/xyz/costamiri/hollowwoods/HollowWoods.java
+++ src/main/java/xyz/costamiri/hollowwoods/HollowWoods.java
@@ -26,6 +26,18 @@ public class HollowWoods implements ModInitializer {
 			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
 	public static final HollowLog HOLLOW_DARK_OAK_LOG
 			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog STRIPPED_HOLLOW_OAK_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog STRIPPED_HOLLOW_SPRUCE_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog STRIPPED_HOLLOW_BIRCH_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog STRIPPED_HOLLOW_JUNGLE_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog STRIPPED_HOLLOW_ACACIA_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
+	public static final HollowLog STRIPPED_HOLLOW_DARK_OAK_LOG
+			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
 
 	@Override
 	public void onInitialize() {
@@ -41,6 +53,18 @@ public class HollowWoods implements ModInitializer {
 				new Identifier(modid, "hollow_acacia_log"), HOLLOW_ACACIA_LOG);
 		Registry.register(Registry.BLOCK,
 				new Identifier(modid, "hollow_dark_oak_log"), HOLLOW_DARK_OAK_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "stripped_hollow_oak_log"), STRIPPED_HOLLOW_OAK_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "stripped_hollow_spruce_log"), STRIPPED_HOLLOW_SPRUCE_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "stripped_hollow_birch_log"), STRIPPED_HOLLOW_BIRCH_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "stripped_hollow_jungle_log"), STRIPPED_HOLLOW_JUNGLE_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "stripped_hollow_acacia_log"), STRIPPED_HOLLOW_ACACIA_LOG);
+		Registry.register(Registry.BLOCK,
+				new Identifier(modid, "stripped_hollow_dark_oak_log"), STRIPPED_HOLLOW_DARK_OAK_LOG);
 
 		Registry.register(Registry.ITEM,
 				new Identifier(modid, "hollow_oak_log"),
@@ -60,5 +84,23 @@ public class HollowWoods implements ModInitializer {
 		Registry.register(Registry.ITEM,
 				new Identifier(modid, "hollow_dark_oak_log"),
 				new BlockItem(HOLLOW_DARK_OAK_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "stripped_hollow_oak_log"),
+				new BlockItem(STRIPPED_HOLLOW_OAK_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "stripped_hollow_spruce_log"),
+				new BlockItem(STRIPPED_HOLLOW_SPRUCE_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "stripped_hollow_birch_log"),
+				new BlockItem(STRIPPED_HOLLOW_BIRCH_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "stripped_hollow_jungle_log"),
+				new BlockItem(STRIPPED_HOLLOW_JUNGLE_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "stripped_hollow_acacia_log"),
+				new BlockItem(STRIPPED_HOLLOW_ACACIA_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+		Registry.register(Registry.ITEM,
+				new Identifier(modid, "stripped_hollow_dark_oak_log"),
+				new BlockItem(STRIPPED_HOLLOW_DARK_OAK_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
 }
