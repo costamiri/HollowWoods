0d186acf4d684b2a2fc763345dd5bf085e9ef754
costamiri <dennisdelacosta@gmail.com>
2021-12-10 18:43:16 +0100

    modid caps

--- src/main/java/xyz/costamiri/hollowwoods/HollowWoods.java
+++ src/main/java/xyz/costamiri/hollowwoods/HollowWoods.java
@@ -12,7 +12,7 @@ import net.minecraft.util.registry.Registry;
 import xyz.costamiri.hollowwoods.blocks.HollowLog;
 
 public class HollowWoods implements ModInitializer {
-	public static final String modid = "hollowwoods";
+	public static final String MODID = "hollowwoods";
 	// public static final Logger LOGGER = LogManager.getLogger("hollowwoods");
 	public static final HollowLog STRIPPED_HOLLOW_OAK_LOG
 			= new HollowLog(FabricBlockSettings.of(Material.WOOD).strength(2.0f).sounds(BlockSoundGroup.WOOD));
@@ -42,65 +42,65 @@ public class HollowWoods implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "hollow_oak_log"), HOLLOW_OAK_LOG);
+				new Identifier(MODID, "hollow_oak_log"), HOLLOW_OAK_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "hollow_spruce_log"), HOLLOW_SPRUCE_LOG);
+				new Identifier(MODID, "hollow_spruce_log"), HOLLOW_SPRUCE_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "hollow_birch_log"), HOLLOW_BIRCH_LOG);
+				new Identifier(MODID, "hollow_birch_log"), HOLLOW_BIRCH_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "hollow_jungle_log"), HOLLOW_JUNGLE_LOG);
+				new Identifier(MODID, "hollow_jungle_log"), HOLLOW_JUNGLE_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "hollow_acacia_log"), HOLLOW_ACACIA_LOG);
+				new Identifier(MODID, "hollow_acacia_log"), HOLLOW_ACACIA_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "hollow_dark_oak_log"), HOLLOW_DARK_OAK_LOG);
+				new Identifier(MODID, "hollow_dark_oak_log"), HOLLOW_DARK_OAK_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "stripped_hollow_oak_log"), STRIPPED_HOLLOW_OAK_LOG);
+				new Identifier(MODID, "stripped_hollow_oak_log"), STRIPPED_HOLLOW_OAK_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "stripped_hollow_spruce_log"), STRIPPED_HOLLOW_SPRUCE_LOG);
+				new Identifier(MODID, "stripped_hollow_spruce_log"), STRIPPED_HOLLOW_SPRUCE_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "stripped_hollow_birch_log"), STRIPPED_HOLLOW_BIRCH_LOG);
+				new Identifier(MODID, "stripped_hollow_birch_log"), STRIPPED_HOLLOW_BIRCH_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "stripped_hollow_jungle_log"), STRIPPED_HOLLOW_JUNGLE_LOG);
+				new Identifier(MODID, "stripped_hollow_jungle_log"), STRIPPED_HOLLOW_JUNGLE_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "stripped_hollow_acacia_log"), STRIPPED_HOLLOW_ACACIA_LOG);
+				new Identifier(MODID, "stripped_hollow_acacia_log"), STRIPPED_HOLLOW_ACACIA_LOG);
 		Registry.register(Registry.BLOCK,
-				new Identifier(modid, "stripped_hollow_dark_oak_log"), STRIPPED_HOLLOW_DARK_OAK_LOG);
+				new Identifier(MODID, "stripped_hollow_dark_oak_log"), STRIPPED_HOLLOW_DARK_OAK_LOG);
 
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "hollow_oak_log"),
+				new Identifier(MODID, "hollow_oak_log"),
 				new BlockItem(HOLLOW_OAK_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "hollow_spruce_log"),
+				new Identifier(MODID, "hollow_spruce_log"),
 				new BlockItem(HOLLOW_SPRUCE_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "hollow_birch_log"),
+				new Identifier(MODID, "hollow_birch_log"),
 				new BlockItem(HOLLOW_BIRCH_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "hollow_jungle_log"),
+				new Identifier(MODID, "hollow_jungle_log"),
 				new BlockItem(HOLLOW_JUNGLE_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "hollow_acacia_log"),
+				new Identifier(MODID, "hollow_acacia_log"),
 				new BlockItem(HOLLOW_ACACIA_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "hollow_dark_oak_log"),
+				new Identifier(MODID, "hollow_dark_oak_log"),
 				new BlockItem(HOLLOW_DARK_OAK_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "stripped_hollow_oak_log"),
+				new Identifier(MODID, "stripped_hollow_oak_log"),
 				new BlockItem(STRIPPED_HOLLOW_OAK_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "stripped_hollow_spruce_log"),
+				new Identifier(MODID, "stripped_hollow_spruce_log"),
 				new BlockItem(STRIPPED_HOLLOW_SPRUCE_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "stripped_hollow_birch_log"),
+				new Identifier(MODID, "stripped_hollow_birch_log"),
 				new BlockItem(STRIPPED_HOLLOW_BIRCH_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "stripped_hollow_jungle_log"),
+				new Identifier(MODID, "stripped_hollow_jungle_log"),
 				new BlockItem(STRIPPED_HOLLOW_JUNGLE_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "stripped_hollow_acacia_log"),
+				new Identifier(MODID, "stripped_hollow_acacia_log"),
 				new BlockItem(STRIPPED_HOLLOW_ACACIA_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 		Registry.register(Registry.ITEM,
-				new Identifier(modid, "stripped_hollow_dark_oak_log"),
+				new Identifier(MODID, "stripped_hollow_dark_oak_log"),
 				new BlockItem(STRIPPED_HOLLOW_DARK_OAK_LOG, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
 }
