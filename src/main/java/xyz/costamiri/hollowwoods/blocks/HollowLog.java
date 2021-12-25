a461f21832679ba93e42590e24656b2168d7d92c
costamiri <dennisdelacosta@gmail.com>
2021-12-25 01:26:24 +0100

    1.16 backport

--- src/main/java/xyz/costamiri/hollowwoods/blocks/HollowLog.java
+++ src/main/java/xyz/costamiri/hollowwoods/blocks/HollowLog.java
@@ -53,7 +53,7 @@ public class HollowLog extends PillarBlock implements Waterloggable {
     @SuppressWarnings("deprecation")
     public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
         if (state.get(WATERLOGGED)) {
-            world.createAndScheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+            world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
         }
         return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
