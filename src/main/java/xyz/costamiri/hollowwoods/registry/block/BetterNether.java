7a4efb95da9b39d83d2e16434c56965a3051f755
costamiri <dennisdelacosta@gmail.com>
2021-12-30 18:55:11 +0100

    moved betternether-related-blocks to own folder

--- src/main/java/xyz/costamiri/hollowwoods/registry/block/BetterNether.java
+++ src/main/java/xyz/costamiri/hollowwoods/registry/block/BetterNether.java
@@ -29,38 +29,38 @@ public class BetterNether {
     }
 
     public static void registerBlocks() {
-        registerLog(HOLLOW_ANCHOR_TREE_LOG, "hollow_anchor_tree_log", false);
-        registerLog(HOLLOW_MUSHROOM_FIR_LOG, "hollow_mushroom_fir_log", false);
-        registerLog(HOLLOW_NETHER_SAKURA_LOG, "hollow_nether_sakura_log", false);
-        registerLog(HOLLOW_RUBEUS_LOG, "hollow_rubeus_log", false);
-        registerLog(HOLLOW_STALAGNATE_LOG, "hollow_stalagnate_log", false);
-        registerLog(HOLLOW_WART_LOG, "hollow_wart_log", false);
-        registerLog(HOLLOW_WILLOW_LOG, "hollow_willow_log", false);
-        registerLog(STRIPPED_HOLLOW_ANCHOR_TREE_LOG, "stripped_hollow_anchor_tree_log", false);
-        registerLog(STRIPPED_HOLLOW_MUSHROOM_FIR_LOG, "stripped_hollow_mushroom_fir_log", false);
-        registerLog(STRIPPED_HOLLOW_NETHER_SAKURA_LOG, "stripped_hollow_nether_sakura_log", false);
-        registerLog(STRIPPED_HOLLOW_RUBEUS_LOG, "stripped_hollow_rubeus_log", false);
-        registerLog(STRIPPED_HOLLOW_STALAGNATE_LOG, "stripped_hollow_stalagnate_log", false);
-        registerLog(STRIPPED_HOLLOW_WART_LOG, "stripped_hollow_wart_log", false);
-        registerLog(STRIPPED_HOLLOW_WILLOW_LOG, "stripped_hollow_willow_log", false);
+        registerLog(HOLLOW_ANCHOR_TREE_LOG, "betternether/hollow_anchor_tree_log", false);
+        registerLog(HOLLOW_MUSHROOM_FIR_LOG, "betternether/hollow_mushroom_fir_log", false);
+        registerLog(HOLLOW_NETHER_SAKURA_LOG, "betternether/hollow_nether_sakura_log", false);
+        registerLog(HOLLOW_RUBEUS_LOG, "betternether/hollow_rubeus_log", false);
+        registerLog(HOLLOW_STALAGNATE_LOG, "betternether/hollow_stalagnate_log", false);
+        registerLog(HOLLOW_WART_LOG, "betternether/hollow_wart_log", false);
+        registerLog(HOLLOW_WILLOW_LOG, "betternether/hollow_willow_log", false);
+        registerLog(STRIPPED_HOLLOW_ANCHOR_TREE_LOG, "betternether/stripped_hollow_anchor_tree_log", false);
+        registerLog(STRIPPED_HOLLOW_MUSHROOM_FIR_LOG, "betternether/stripped_hollow_mushroom_fir_log", false);
+        registerLog(STRIPPED_HOLLOW_NETHER_SAKURA_LOG, "betternether/stripped_hollow_nether_sakura_log", false);
+        registerLog(STRIPPED_HOLLOW_RUBEUS_LOG, "betternether/stripped_hollow_rubeus_log", false);
+        registerLog(STRIPPED_HOLLOW_STALAGNATE_LOG, "betternether/stripped_hollow_stalagnate_log", false);
+        registerLog(STRIPPED_HOLLOW_WART_LOG, "betternether/stripped_hollow_wart_log", false);
+        registerLog(STRIPPED_HOLLOW_WILLOW_LOG, "betternether/stripped_hollow_willow_log", false);
     }
 
     public static void addAllRecipes() {
-        addRecipesForLog("hollow_anchor_tree_log", "anchor_tree_log", "anchor_tree_planks");
-        addRecipesForLog("hollow_mushroom_fir_log", "mushroom_fir_log", "mushroom_fir_planks");
-        addRecipesForLog("hollow_nether_sakura_log", "nether_sakura_log", "nether_sakura_planks");
-        addRecipesForLog("hollow_rubeus_log", "rubeus_log", "rubeus_planks");
-        addRecipesForLog("hollow_stalagnate_log", "stalagnate_log", "stalagnate_planks");
-        addRecipesForLog("hollow_wart_log", "wart_log", "wart_planks");
-        addRecipesForLog("hollow_willow_log", "willow_log", "willow_planks");
+        addRecipesForLog("betternether/hollow_anchor_tree_log", "anchor_tree_log", "anchor_tree_planks");
+        addRecipesForLog("betternether/hollow_mushroom_fir_log", "mushroom_fir_log", "mushroom_fir_planks");
+        addRecipesForLog("betternether/hollow_nether_sakura_log", "nether_sakura_log", "nether_sakura_planks");
+        addRecipesForLog("betternether/hollow_rubeus_log", "rubeus_log", "rubeus_planks");
+        addRecipesForLog("betternether/hollow_stalagnate_log", "stalagnate_log", "stalagnate_planks");
+        addRecipesForLog("betternether/hollow_wart_log", "wart_log", "wart_planks");
+        addRecipesForLog("betternether/hollow_willow_log", "willow_log", "willow_planks");
 
-        addRecipesForLog("stripped_hollow_anchor_tree_log", "anchor_tree_stripped_log", "anchor_tree_planks");
-        addRecipesForLog("stripped_hollow_mushroom_fir_log", "mushroom_fir_stripped_log", "mushroom_fir_planks");
-        addRecipesForLog("stripped_hollow_nether_sakura_log", "nether_sakura_stripped_log", "nether_sakura_planks");
-        addRecipesForLog("stripped_hollow_rubeus_log", "rubeus_stripped_log", "rubeus_planks");
-        addRecipesForLog("stripped_hollow_stalagnate_log", "stalagnate_stripped_log", "stalagnate_planks");
-        addRecipesForLog("stripped_hollow_wart_log", "wart_stripped_log", "wart_planks");
-        addRecipesForLog("stripped_hollow_willow_log", "willow_stripped_log", "willow_planks");
+        addRecipesForLog("betternether/stripped_hollow_anchor_tree_log", "anchor_tree_stripped_log", "anchor_tree_planks");
+        addRecipesForLog("betternether/stripped_hollow_mushroom_fir_log", "mushroom_fir_stripped_log", "mushroom_fir_planks");
+        addRecipesForLog("betternether/stripped_hollow_nether_sakura_log", "nether_sakura_stripped_log", "nether_sakura_planks");
+        addRecipesForLog("betternether/stripped_hollow_rubeus_log", "rubeus_stripped_log", "rubeus_planks");
+        addRecipesForLog("betternether/stripped_hollow_stalagnate_log", "stalagnate_stripped_log", "stalagnate_planks");
+        addRecipesForLog("betternether/stripped_hollow_wart_log", "wart_stripped_log", "wart_planks");
+        addRecipesForLog("betternether/stripped_hollow_willow_log", "willow_stripped_log", "willow_planks");
     }
 
     private static void addRecipesForLog(String hwLog, String bnLog, String bnPlanks) {
