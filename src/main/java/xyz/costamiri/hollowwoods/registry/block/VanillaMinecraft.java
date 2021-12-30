2dc68115cdd7edabb7cecba7e14815e1c8cca95b
costamiri <dennisdelacosta@gmail.com>
2021-12-30 19:23:14 +0100

    added Vanilla Hollow logs to own RecipeManager

--- src/main/java/xyz/costamiri/hollowwoods/registry/block/VanillaMinecraft.java
+++ src/main/java/xyz/costamiri/hollowwoods/registry/block/VanillaMinecraft.java
@@ -1,7 +1,10 @@
 package xyz.costamiri.hollowwoods.registry.block;
 
+import net.minecraft.util.Identifier;
 import xyz.costamiri.hollowwoods.blocks.HollowLog;
+import xyz.costamiri.hollowwoods.recipes.HWRecipeManager;
 
+import static xyz.costamiri.hollowwoods.HollowWoods.MODID;
 import static xyz.costamiri.hollowwoods.HollowWoods.registerLog;
 
 public class VanillaMinecraft {
@@ -23,6 +26,11 @@ public class VanillaMinecraft {
     public static final HollowLog HOLLOW_WARPED_STEM = new HollowLog(STRIPPED_HOLLOW_WARPED_STEM);
 
     public static void init() {
+        registerBlocks();
+        addAllRecipes();
+    }
+
+    public static void registerBlocks() {
         registerLog(HOLLOW_OAK_LOG, "hollow_oak_log");
         registerLog(HOLLOW_SPRUCE_LOG, "hollow_spruce_log");
         registerLog(HOLLOW_BIRCH_LOG, "hollow_birch_log");
@@ -39,5 +47,29 @@ public class VanillaMinecraft {
         registerLog(STRIPPED_HOLLOW_DARK_OAK_LOG, "stripped_hollow_dark_oak_log");
         registerLog(STRIPPED_HOLLOW_CRIMSON_STEM, "stripped_hollow_crimson_stem", false);
         registerLog(STRIPPED_HOLLOW_WARPED_STEM, "stripped_hollow_warped_stem", false);
+    }
+
+    private static void addAllRecipes() {
+        addRecipesForLog("hollow_oak_log", "oak_log", "oak_planks");
+        addRecipesForLog("hollow_spruce_log", "spruce_log", "spruce_planks");
+        addRecipesForLog("hollow_birch_log", "birch_log", "birch_planks");
+        addRecipesForLog("hollow_jungle_log", "jungle_log", "jungle_planks");
+        addRecipesForLog("hollow_acacia_log", "acacia_log", "acacia_planks");
+        addRecipesForLog("hollow_dark_oak_log", "dark_oak_log", "dark_oak_planks");
+        addRecipesForLog("hollow_crimson_stem", "crimson_stem", "crimson_planks");
+        addRecipesForLog("hollow_warped_stem", "warped_stem", "warped_planks");
+        addRecipesForLog("stripped_hollow_oak_log", "stripped_oak_log", "oak_planks");
+        addRecipesForLog("stripped_hollow_spruce_log", "stripped_spruce_log", "spruce_planks");
+        addRecipesForLog("stripped_hollow_birch_log", "stripped_birch_log", "birch_planks");
+        addRecipesForLog("stripped_hollow_jungle_log", "stripped_jungle_log", "jungle_planks");
+        addRecipesForLog("stripped_hollow_acacia_log", "stripped_acacia_log", "acacia_planks");
+        addRecipesForLog("stripped_hollow_dark_oak_log", "stripped_dark_oak_log", "dark_oak_planks");
+        addRecipesForLog("stripped_hollow_crimson_stem", "stripped_crimson_stem", "crimson_planks");
+        addRecipesForLog("stripped_hollow_warped_stem", "stripped_warped_stem", "warped_planks");
+    }
+
+    private static void addRecipesForLog(String hwLog, String mcLog, String mcPlanks) {
+        HWRecipeManager.addShapelessRecipe(new Identifier(MODID, hwLog), new Identifier("minecraft", mcPlanks), 2);
+        HWRecipeManager.addStonecuttingRecipe(new Identifier("minecraft", mcLog), new Identifier(MODID, hwLog), 1);
     }
 }
