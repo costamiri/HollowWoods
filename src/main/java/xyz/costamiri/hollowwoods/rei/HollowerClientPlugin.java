7d067ce0d179c5da83292b3beafa6885105c7f87
costamiri <dennisdelacosta@gmail.com>
2022-03-28 09:44:18 +0200

    added all hollower tools to rei

--- src/main/java/xyz/costamiri/hollowwoods/rei/HollowerClientPlugin.java
+++ src/main/java/xyz/costamiri/hollowwoods/rei/HollowerClientPlugin.java
@@ -4,8 +4,10 @@ import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.minecraft.util.Identifier;
 import xyz.costamiri.hollowwoods.recipes.HollowerRecipe;
+import xyz.costamiri.hollowwoods.registry.item.VanillaMinecraftItems;
 
 import static xyz.costamiri.hollowwoods.HollowWoods.MODID;
 
@@ -15,7 +17,12 @@ public class HollowerClientPlugin implements REIClientPlugin {
     @Override
     public void registerCategories(CategoryRegistry registry) {
         registry.add(new HollowerCategory());
-        registry.addWorkstations(HOLLOWING, HollowerCategory.ICON);
+        registry.addWorkstations(HOLLOWING, EntryStacks.of(VanillaMinecraftItems.WOODEN_HOLLOWER));
+        registry.addWorkstations(HOLLOWING, EntryStacks.of(VanillaMinecraftItems.STONE_HOLLOWER));
+        registry.addWorkstations(HOLLOWING, EntryStacks.of(VanillaMinecraftItems.GOLDEN_HOLLOWER));
+        registry.addWorkstations(HOLLOWING, EntryStacks.of(VanillaMinecraftItems.IRON_HOLLOWER));
+        registry.addWorkstations(HOLLOWING, EntryStacks.of(VanillaMinecraftItems.DIAMOND_HOLLOWER));
+        registry.addWorkstations(HOLLOWING, EntryStacks.of(VanillaMinecraftItems.NETHERITE_HOLLOWER));
         registry.removePlusButton(HOLLOWING);
     }
 
