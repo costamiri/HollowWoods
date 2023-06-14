b1c495b229b229e46958c6930bcec36404d3b241
costamiri <dennisdelacosta@gmail.com>
2023-06-14 16:35:32 +0200

    new promenade logs

--- src/main/java/xyz/costamiri/hollowwoods/registry/block/Promenade.java
+++ src/main/java/xyz/costamiri/hollowwoods/registry/block/Promenade.java
@@ -2,12 +2,17 @@ package xyz.costamiri.hollowwoods.registry.block;
 
 import net.minecraft.util.Identifier;
 import xyz.costamiri.hollowwoods.blocks.HollowLog;
+import xyz.costamiri.hollowwoods.util.LogTextureMap;
 
 public class Promenade extends HollowBlocks {
-    public static final HollowLog HOLLOW_CHERRY_OAK_LOG = new HollowLog();
+    public static final HollowLog HOLLOW_DARK_AMARANTH_STEM = new HollowLog();
+    public static final HollowLog HOLLOW_MAPLE_LOG = new HollowLog();
     public static final HollowLog HOLLOW_PALM_LOG = new HollowLog();
-    public static final HollowLog STRIPPED_HOLLOW_CHERRY_OAK_LOG = new HollowLog();
+    public static final HollowLog HOLLOW_SAKURA_LOG = new HollowLog();
+    public static final HollowLog STRIPPED_HOLLOW_DARK_AMARANTH_STEM = new HollowLog();
+    public static final HollowLog STRIPPED_HOLLOW_MAPLE_LOG = new HollowLog();
     public static final HollowLog STRIPPED_HOLLOW_PALM_LOG = new HollowLog();
+    public static final HollowLog STRIPPED_HOLLOW_SAKURA_LOG = new HollowLog();
 
     public Promenade() {
         this.namespace = "promenade";
@@ -16,12 +21,32 @@ public class Promenade extends HollowBlocks {
     @Override
     public void registerBlocks() {
         createLog(
-                HOLLOW_CHERRY_OAK_LOG,
-                STRIPPED_HOLLOW_CHERRY_OAK_LOG,
-                "cherry_oak_log",
-                new Identifier(namespace, "cherry_oak_log"),
-                new Identifier(namespace, "stripped_cherry_oak_log"),
-                new Identifier(namespace, "cherry_oak_planks")
+                HOLLOW_DARK_AMARANTH_STEM,
+                STRIPPED_HOLLOW_DARK_AMARANTH_STEM,
+                "dark_amaranth_stem",
+                new Identifier(namespace, "dark_amaranth_stem"),
+                new Identifier(namespace, "stripped_dark_amaranth_stem"),
+                new Identifier(namespace, "dark_amaranth_planks"),
+                new LogTextureMap(
+                        new Identifier(namespace, "block/dark_amaranth/stem/side"),
+                        new Identifier(namespace, "block/dark_amaranth/stripped_stem/side"),
+                        new Identifier(namespace, "block/dark_amaranth/stem/top"),
+                        new Identifier(namespace, "block/dark_amaranth/stripped_stem/top")
+                )
+        );
+        createLog(
+                HOLLOW_MAPLE_LOG,
+                STRIPPED_HOLLOW_MAPLE_LOG,
+                "maple_log",
+                new Identifier(namespace, "maple_log"),
+                new Identifier(namespace, "stripped_maple_log"),
+                new Identifier(namespace, "maple_planks"),
+                new LogTextureMap(
+                        new Identifier(namespace, "block/maple/log/side"),
+                        new Identifier(namespace, "block/maple/stripped_log/side"),
+                        new Identifier(namespace, "block/maple/log/top"),
+                        new Identifier(namespace, "block/maple/stripped_log/top")
+                )
         );
         createLog(
                 HOLLOW_PALM_LOG,
@@ -29,7 +54,27 @@ public class Promenade extends HollowBlocks {
                 "palm_log",
                 new Identifier(namespace, "palm_log"),
                 new Identifier(namespace, "stripped_palm_log"),
-                new Identifier(namespace, "palm_planks")
+                new Identifier(namespace, "palm_planks"),
+                new LogTextureMap(
+                        new Identifier(namespace, "block/palm/log/side"),
+                        new Identifier(namespace, "block/palm/stripped_log/side"),
+                        new Identifier(namespace, "block/palm/log/top"),
+                        new Identifier(namespace, "block/palm/stripped_log/top")
+                )
+        );
+        createLog(
+                HOLLOW_SAKURA_LOG,
+                STRIPPED_HOLLOW_SAKURA_LOG,
+                "sakura_log",
+                new Identifier(namespace, "sakura_log"),
+                new Identifier(namespace, "stripped_sakura_log"),
+                new Identifier(namespace, "sakura_planks"),
+                new LogTextureMap(
+                        new Identifier(namespace, "block/sakura/log/side"),
+                        new Identifier(namespace, "block/sakura/stripped_log/side"),
+                        new Identifier(namespace, "block/sakura/log/top"),
+                        new Identifier(namespace, "block/sakura/stripped_log/top")
+                )
         );
     }
 }
