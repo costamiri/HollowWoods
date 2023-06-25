8f1c11b52bd0c0f71c134b351a8dbe1bb90e8602
costamiri <dennisdelacosta@gmail.com>
2023-06-25 11:44:29 +0200

    terrestria map colors

--- src/main/java/xyz/costamiri/hollowwoods/registry/block/Terrestria.java
+++ src/main/java/xyz/costamiri/hollowwoods/registry/block/Terrestria.java
@@ -1,5 +1,6 @@
 package xyz.costamiri.hollowwoods.registry.block;
 
+import net.minecraft.block.MapColor;
 import net.minecraft.util.Identifier;
 import xyz.costamiri.hollowwoods.blocks.HollowLog;
 
@@ -25,56 +26,56 @@ public class Terrestria extends HollowBlocks {
 
     @Override
     public void registerBlocks() {
-        HOLLOW_CYPRESS_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_CYPRESS_LOG = new HollowLog.Builder().build();
+        HOLLOW_CYPRESS_LOG = new HollowLog.Builder().mapColor(MapColor.LIGHT_GRAY, MapColor.TERRACOTTA_WHITE).build();
+        STRIPPED_HOLLOW_CYPRESS_LOG = new HollowLog.Builder().mapColor(MapColor.LIGHT_GRAY).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_CYPRESS_LOG, new Identifier(this.namespace, "cypress_log"))
                 .stripped(STRIPPED_HOLLOW_CYPRESS_LOG, new Identifier(this.namespace, "stripped_cypress_log"))
                 .planks(new Identifier(this.namespace, "cypress_planks"))
                 .create();
 
-        HOLLOW_HEMLOCK_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_HEMLOCK_LOG = new HollowLog.Builder().build();
+        HOLLOW_HEMLOCK_LOG = new HollowLog.Builder().mapColor(MapColor.OAK_TAN, MapColor.BROWN).build();
+        STRIPPED_HOLLOW_HEMLOCK_LOG = new HollowLog.Builder().mapColor(MapColor.OAK_TAN).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_HEMLOCK_LOG, new Identifier(this.namespace, "hemlock_log"))
                 .stripped(STRIPPED_HOLLOW_HEMLOCK_LOG, new Identifier(this.namespace, "stripped_hemlock_log"))
                 .planks(new Identifier(this.namespace, "hemlock_planks"))
                 .create();
 
-        HOLLOW_JAPANESE_MAPLE_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_JAPANESE_MAPLE_LOG = new HollowLog.Builder().build();
+        HOLLOW_JAPANESE_MAPLE_LOG = new HollowLog.Builder().mapColor(MapColor.TERRACOTTA_MAGENTA, MapColor.BROWN).build();
+        STRIPPED_HOLLOW_JAPANESE_MAPLE_LOG = new HollowLog.Builder().mapColor(MapColor.TERRACOTTA_MAGENTA).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_JAPANESE_MAPLE_LOG, new Identifier(this.namespace, "japanese_maple_log"))
                 .stripped(STRIPPED_HOLLOW_JAPANESE_MAPLE_LOG, new Identifier(this.namespace, "stripped_japanese_maple_log"))
                 .planks(new Identifier(this.namespace, "japanese_maple_planks"))
                 .create();
 
-        HOLLOW_RAINBOW_EUCALYPTUS_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_RAINBOW_EUCALYPTUS_LOG = new HollowLog.Builder().build();
+        HOLLOW_RAINBOW_EUCALYPTUS_LOG = new HollowLog.Builder().mapColor(MapColor.LAPIS_BLUE, MapColor.BLUE).build();
+        STRIPPED_HOLLOW_RAINBOW_EUCALYPTUS_LOG = new HollowLog.Builder().mapColor(MapColor.LAPIS_BLUE).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_RAINBOW_EUCALYPTUS_LOG, new Identifier(this.namespace, "rainbow_eucalyptus_log"))
                 .stripped(STRIPPED_HOLLOW_RAINBOW_EUCALYPTUS_LOG, new Identifier(this.namespace, "stripped_rainbow_eucalyptus_log"))
                 .planks(new Identifier(this.namespace, "rainbow_eucalyptus_planks"))
                 .create();
 
-        HOLLOW_REDWOOD_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_REDWOOD_LOG = new HollowLog.Builder().build();
+        HOLLOW_REDWOOD_LOG = new HollowLog.Builder().mapColor(MapColor.OAK_TAN, MapColor.TERRACOTTA_RED).build();
+        STRIPPED_HOLLOW_REDWOOD_LOG = new HollowLog.Builder().mapColor(MapColor.OAK_TAN).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_REDWOOD_LOG, new Identifier(this.namespace, "redwood_log"))
                 .stripped(STRIPPED_HOLLOW_REDWOOD_LOG, new Identifier(this.namespace, "stripped_redwood_log"))
                 .planks(new Identifier(this.namespace, "redwood_planks"))
                 .create();
 
-        HOLLOW_RUBBER_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_RUBBER_LOG = new HollowLog.Builder().build();
+        HOLLOW_RUBBER_LOG = new HollowLog.Builder().mapColor(MapColor.PALE_YELLOW, MapColor.TERRACOTTA_WHITE).build();
+        STRIPPED_HOLLOW_RUBBER_LOG = new HollowLog.Builder().mapColor(MapColor.PALE_YELLOW).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_RUBBER_LOG, new Identifier(this.namespace, "rubber_log"))
                 .stripped(STRIPPED_HOLLOW_RUBBER_LOG, new Identifier(this.namespace, "stripped_rubber_log"))
                 .planks(new Identifier(this.namespace, "rubber_planks"))
                 .create();
 
-        HOLLOW_WILLOW_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_WILLOW_LOG = new HollowLog.Builder().build();
+        HOLLOW_WILLOW_LOG = new HollowLog.Builder().mapColor(MapColor.GRAY, MapColor.TERRACOTTA_WHITE).build();
+        STRIPPED_HOLLOW_WILLOW_LOG = new HollowLog.Builder().mapColor(MapColor.GRAY).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_WILLOW_LOG, new Identifier(this.namespace, "willow_log"))
                 .stripped(STRIPPED_HOLLOW_WILLOW_LOG, new Identifier(this.namespace, "stripped_willow_log"))
