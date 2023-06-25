b3cba5b3ba31bb21c48def7c7b8f90fe169399e3
costamiri <dennisdelacosta@gmail.com>
2023-06-25 12:25:41 +0200

    blockus map color

--- src/main/java/xyz/costamiri/hollowwoods/registry/block/Blockus.java
+++ src/main/java/xyz/costamiri/hollowwoods/registry/block/Blockus.java
@@ -1,5 +1,6 @@
 package xyz.costamiri.hollowwoods.registry.block;
 
+import net.minecraft.block.MapColor;
 import net.minecraft.util.Identifier;
 import xyz.costamiri.hollowwoods.blocks.HollowLog;
 
@@ -13,8 +14,8 @@ public class Blockus extends HollowBlocks {
 
     @Override
     public void registerBlocks() {
-        HOLLOW_WHITE_OAK_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_WHITE_OAK_LOG = new HollowLog.Builder().build();
+        HOLLOW_WHITE_OAK_LOG = new HollowLog.Builder().mapColor(MapColor.TERRACOTTA_LIGHT_GRAY).build();
+        STRIPPED_HOLLOW_WHITE_OAK_LOG = new HollowLog.Builder().mapColor(MapColor.TERRACOTTA_LIGHT_GRAY).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_WHITE_OAK_LOG, new Identifier(this.namespace, "white_oak_log"))
                 .stripped(STRIPPED_HOLLOW_WHITE_OAK_LOG, new Identifier(this.namespace, "stripped_white_oak_log"))
