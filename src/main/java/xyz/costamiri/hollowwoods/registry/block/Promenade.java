2dd2b66c3cb88acf5faaceeef5c7c59397082e8c
costamiri <dennisdelacosta@gmail.com>
2023-06-25 11:50:08 +0200

    promenade map colors + sounds

--- src/main/java/xyz/costamiri/hollowwoods/registry/block/Promenade.java
+++ src/main/java/xyz/costamiri/hollowwoods/registry/block/Promenade.java
@@ -1,5 +1,7 @@
 package xyz.costamiri.hollowwoods.registry.block;
 
+import net.minecraft.block.MapColor;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import xyz.costamiri.hollowwoods.blocks.HollowLog;
 import xyz.costamiri.hollowwoods.util.LogTextureMap;
@@ -20,8 +22,8 @@ public class Promenade extends HollowBlocks {
 
     @Override
     public void registerBlocks() {
-        HOLLOW_DARK_AMARANTH_STEM = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_DARK_AMARANTH_STEM = new HollowLog.Builder().build();
+        HOLLOW_DARK_AMARANTH_STEM = new HollowLog.Builder().mapColor(MapColor.GRAY, MapColor.DARK_DULL_PINK).sounds(BlockSoundGroup.NETHER_WOOD).build();
+        STRIPPED_HOLLOW_DARK_AMARANTH_STEM = new HollowLog.Builder().mapColor(MapColor.GRAY).sounds(BlockSoundGroup.NETHER_WOOD).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_DARK_AMARANTH_STEM, new Identifier(this.namespace, "dark_amaranth_stem"))
                 .stripped(STRIPPED_HOLLOW_DARK_AMARANTH_STEM, new Identifier(this.namespace, "stripped_dark_amaranth_stem"))
@@ -33,8 +35,8 @@ public class Promenade extends HollowBlocks {
                         new Identifier(this.namespace, "block/dark_amaranth/stripped_stem/top")
                 ))
                 .create();
-        HOLLOW_MAPLE_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_MAPLE_LOG = new HollowLog.Builder().build();
+        HOLLOW_MAPLE_LOG = new HollowLog.Builder().mapColor(MapColor.TERRACOTTA_WHITE, MapColor.DEEPSLATE_GRAY).build();
+        STRIPPED_HOLLOW_MAPLE_LOG = new HollowLog.Builder().mapColor(MapColor.TERRACOTTA_WHITE).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_MAPLE_LOG, new Identifier(this.namespace, "maple_log"))
                 .stripped(STRIPPED_HOLLOW_MAPLE_LOG, new Identifier(this.namespace, "stripped_maple_log"))
@@ -46,8 +48,8 @@ public class Promenade extends HollowBlocks {
                         new Identifier(this.namespace, "block/maple/stripped_log/top")
                 ))
                 .create();
-        HOLLOW_PALM_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_PALM_LOG = new HollowLog.Builder().build();
+        HOLLOW_PALM_LOG = new HollowLog.Builder().mapColor(MapColor.ORANGE, MapColor.SPRUCE_BROWN).build();
+        STRIPPED_HOLLOW_PALM_LOG = new HollowLog.Builder().mapColor(MapColor.ORANGE).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_PALM_LOG, new Identifier(this.namespace, "palm_log"))
                 .stripped(STRIPPED_HOLLOW_PALM_LOG, new Identifier(this.namespace, "stripped_palm_log"))
@@ -59,8 +61,8 @@ public class Promenade extends HollowBlocks {
                         new Identifier(this.namespace, "block/palm/stripped_log/top")
                 ))
                 .create();
-        HOLLOW_SAKURA_LOG = new HollowLog.Builder().build();
-        STRIPPED_HOLLOW_SAKURA_LOG = new HollowLog.Builder().build();
+        HOLLOW_SAKURA_LOG = new HollowLog.Builder().mapColor(MapColor.TERRACOTTA_BROWN).sounds(BlockSoundGroup.CHERRY_WOOD).build();
+        STRIPPED_HOLLOW_SAKURA_LOG = new HollowLog.Builder().mapColor(MapColor.TERRACOTTA_BROWN).sounds(BlockSoundGroup.CHERRY_WOOD).build();
         new HollowBlocks.LogCreator()
                 .raw(HOLLOW_SAKURA_LOG, new Identifier(this.namespace, "sakura_log"))
                 .stripped(STRIPPED_HOLLOW_SAKURA_LOG, new Identifier(this.namespace, "stripped_sakura_log"))
